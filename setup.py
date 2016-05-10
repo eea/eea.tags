19431970fdede038d0622826058a9d33cf8dd286
ichim-david <ichim.david@gmail.com>
2016-05-10 15:49:26 +0300

    * Bug fix: Fix pylint  warnings for eea.tags [ichim-david refs #71940]

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 """ Installer
 """
-from setuptools import setup, find_packages
 import os
 from os.path import join
+from setuptools import setup, find_packages
 
 NAME = 'eea.tags'
 PATH = NAME.split('.') + ['version.txt']
